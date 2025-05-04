dc45daac36d141f7f39d09f9e1a476bccd0b1933
moloudii <mhmmdalinaz@gmail.com>
2025-05-04 17:53:42 +0330

    notFound page

--- src/app/store/[id]/page.tsx
+++ src/app/store/[id]/page.tsx
@@ -1,10 +1,14 @@
+import { notFound } from "next/navigation";
+
 interface IProductProps {
   params: Promise<{ id: string }>;
   searchParams: Promise<object>;
 }
 async function Product(props: IProductProps) {
   const { id } = await props.params;
-  console.log(id);
+  if (parseInt(id) > 100) {
+    return notFound();
+  }
 
   return <div>Product: {id}</div>;
 }
