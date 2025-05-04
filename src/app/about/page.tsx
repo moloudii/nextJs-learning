326ad86ccf74506aedc187b8f4453ca09c953c03
moloudii <mhmmdalinaz@gmail.com>
2025-05-04 19:54:56 +0330

    metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,9 @@
+import { Metadata } from "next";
 import React from "react";
 
+export const metadata: Metadata = {
+  title: "About Page",
+};
 const about = () => {
   return <div>about</div>;
 };
--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,3 +1,8 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: { absolute: "Products" },
+};
 const page = () => {
   return <div>page</div>;
 };
