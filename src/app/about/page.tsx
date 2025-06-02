d1035ade8c9c2bde60171d54f8c9c9d5a6729787
moloudii <mhmmdalinaz@gmail.com>
2025-06-02 11:54:35 +0330

    data fetch

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,16 +2,39 @@ import { Metadata } from "next";
 import React from "react";
 import Counter from "./component/counter";
 import ServerComp from "./component/serverComp";
+import axios from "axios";
 
 export const metadata: Metadata = {
   title: "About Page",
 };
-const about = () => {
+export interface IGetProduct {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: Rating;
+}
+export interface Rating {
+  rate: number;
+  count: number;
+}
+const about = async () => {
+  const { data } = await axios.get("https://fakestoreapi.com/products");
+  console.log(data);
+
   return (
     <>
       <div>about</div>
       <Counter>
         <ServerComp />
+        {data.map((item: IGetProduct) => (
+          <div key={item.id} className="bg-slate-600 mb-2">
+            <h5>{item.title}</h5>
+            <p>{item.description}</p>
+          </div>
+        ))}
       </Counter>
     </>
   );
